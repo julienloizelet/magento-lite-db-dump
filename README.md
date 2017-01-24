4f353c5d00b9de62d027236cfb83a45fbfd7b497
Julien Loizelet <jloizelet@sergent-major.com>
2017-01-24 15:40:25 +0100

    add host option + delete password option + add some file for global and specific configuration

--- README.md
+++ README.md
@@ -27,106 +27,24 @@ Called from the command line, dumper.sh has the following options:
 *   -h help
 *   -d database
 *   -u username
-*   -p password
+*   -H host
 *   -g flag to gzip the resulting file.
 
 The script produces a filename called $dbname.dump.sql(.gz)
 
-The following tables are left empty:
+Tables in 'global-parameters' file are left empty : see GLOBAL_IGNORED_TABLES variable.
 
-*   api_session
-*   core_session
-*   customer_address_entity
-*   customer_address_entity_datetime
-*   customer_address_entity_decimal
-*   customer_address_entity_int
-*   customer_address_entity_text
-*   customer_address_entity_varchar
-*   customer_entity
-*   customer_entity_decimal
-*   customer_entity_int
-*   customer_entity_text
-*   customer_entity_varchar
-*   dataflow_session
-*   enterprise_customer_sales_flat_order
-*   enterprise_customer_sales_flat_order_address
-*   enterprise_customer_sales_flat_quote
-*   enterprise_customer_sales_flat_quote_address
-*   enterprise_customerbalance
-*   enterprise_customerbalance_history
-*   enterprise_customersegment_customer
-*   log_customer
-*   log_quote
-*   log_summary
-*   log_summary_type
-*   log_url
-*   log_url_info
-*   log_visitor
-*   log_visitor_info
-*   log_visitor_online
-*   persistent_session
-*   sales_bestsellers_aggregated_daily
-*   sales_bestsellers_aggregated_monthly
-*   sales_bestsellers_aggregated_yearly
-*   sales_billing_agreement
-*   sales_billing_agreement_order
-*   sales_flat_creditmemo
-*   sales_flat_creditmemo_comment
-*   sales_flat_creditmemo_grid
-*   sales_flat_creditmemo_item
-*   sales_flat_invoice
-*   sales_flat_invoice_comment
-*   sales_flat_invoice_grid
-*   sales_flat_invoice_item
-*   sales_flat_order
-*   sales_flat_order_address
-*   sales_flat_order_grid
-*   sales_flat_order_item
-*   sales_flat_order_payment
-*   sales_flat_order_status_history
-*   sales_flat_quote
-*   sales_flat_quote_address
-*   sales_flat_quote_address_item
-*   sales_flat_quote_item
-*   sales_flat_quote_item_option
-*   sales_flat_quote_payment
-*   sales_flat_quote_shipping_rate
-*   sales_flat_shipment
-*   sales_flat_shipment_comment
-*   sales_flat_shipment_grid
-*   sales_flat_shipment_item
-*   sales_flat_shipment_track
-*   sales_invoiced_aggregated
-*   sales_invoiced_aggregated_order
-*   sales_order_aggregated_created
-*   sales_order_aggregated_updated
-*   sales_order_status
-*   sales_order_status_label
-*   sales_order_status_state
-*   sales_order_tax
-*   sales_order_tax_item
-*   sales_payment_transaction
-*   sales_recurring_profile
-*   sales_recurring_profile_order
-*   sales_refunded_aggregated
-*   sales_refunded_aggregated_order
-*   sales_shipping_aggregated
-*   sales_shipping_aggregated_order
-*   salesrule
-*   salesrule_coupon
-*   salesrule_coupon_usage
-*   salesrule_customer
-*   salesrule_customer_group
-*   salesrule_label
-*   salesrule_product_attribute
-*   salesrule_website
-*   wishlist
-*   wishlist_item
-*   wishlist_item_option
+If you have tables that are specifics to your project (e.g from third parts extension), you should
+create a file called 'project-specific-parameters' with a content like :
+SPECIFIC_IGNORED_TABLES=(
+"my_module_table1"
+
+"my_module_table2"
+
+)
+
 
 Collaboration
 -------------
 
 Your suggestions and pull requests are welcome!
-
-I am pretty new to shell scripting, and there may be other tables in Magento that would be good candidates for skipping.
--- dumper.sh
+++ dumper.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
+. project-specific-parameters
+. global-parameters
+
+
 usage="$(basename "$0"): Dumps Magento database leaving customer, order, log data empty.
 Creates a file called databasename.dump.sql(.gz)
-Works only on localhost.
 
 Usage:
+    -H host
     -d database
     -u username
-    -p password
     -g gzip file on completion
     -h this help"
-
+HOST=
 DBNAME=
 USERNAME=
 PASSWORD=
 GZIPFLAG=0
-while getopts d:u:p:gh opt
+# Get Options
+while getopts d:u:H:gh opt
 do
     case "$opt" in
         d) DBNAME="$OPTARG";;
         u) USERNAME="$OPTARG";;
-        p) PASSWORD="$OPTARG";;
         g) GZIPFLAG=1;;
+        H) HOST="$OPTARG";;
         h) echo "$usage" >&2
            exit 1;;
         ?) printf "illegal option '%s'\n" "$OPTARG" >&2
@@ -28,96 +32,37 @@ do
            exit 1;;
     esac
 done
+# Some checks
+if [ "$HOST" == "" ]; then
+    echo "Provide host: -H HOST" >&2
+    exit 1
+fi
 if [ "$DBNAME" == "" ]; then
     echo "Provide a database name: -d DBNAME" >&2
     exit 1
 fi
+echo -n "Type Password for Database $DBNAME and User $USERNAME : "
+read -s PASSWORD
+
 PASSWORDISOK=`mysqladmin -u$USERNAME -p$PASSWORD ping | grep -c "mysqld is alive"`
 if [ "$PASSWORDISOK" != 1 ]; then
     echo "Could not connect." >&2
     exit 1
 fi
 
-mysqldump -u$USERNAME -p$PASSWORD $DBNAME --no-data > $DBNAME.dump.sql
-mysqldump -u$USERNAME -p$PASSWORD $DBNAME -t  \
-    --ignore-table $DBNAME.log_customer \
-    --ignore-table $DBNAME.log_quote \
-    --ignore-table $DBNAME.log_summary \
-    --ignore-table $DBNAME.log_summary_type \
-    --ignore-table $DBNAME.log_url \
-    --ignore-table $DBNAME.log_url_info \
-    --ignore-table $DBNAME.log_visitor \
-    --ignore-table $DBNAME.log_visitor_info \
-    --ignore-table $DBNAME.log_visitor_online \
-    --ignore-table $DBNAME.api_session \
-    --ignore-table $DBNAME.core_session \
-    --ignore-table $DBNAME.dataflow_session \
-    --ignore-table $DBNAME.persistent_session \
-    --ignore-table $DBNAME.enterprise_customer_sales_flat_order \
-    --ignore-table $DBNAME.enterprise_customer_sales_flat_order_address \
-    --ignore-table $DBNAME.enterprise_customer_sales_flat_quote \
-    --ignore-table $DBNAME.enterprise_customer_sales_flat_quote_address \
-    --ignore-table $DBNAME.enterprise_customerbalance \
-    --ignore-table $DBNAME.enterprise_customerbalance_history \
-    --ignore-table $DBNAME.enterprise_customersegment_customer \
-    --ignore-table $DBNAME.sales_bestsellers_aggregated_daily \
-    --ignore-table $DBNAME.sales_bestsellers_aggregated_monthly \
-    --ignore-table $DBNAME.sales_bestsellers_aggregated_yearly \
-    --ignore-table $DBNAME.sales_billing_agreement \
-    --ignore-table $DBNAME.sales_billing_agreement_order \
-    --ignore-table $DBNAME.sales_flat_creditmemo \
-    --ignore-table $DBNAME.sales_flat_creditmemo_comment \
-    --ignore-table $DBNAME.sales_flat_creditmemo_grid \
-    --ignore-table $DBNAME.sales_flat_creditmemo_item \
-    --ignore-table $DBNAME.sales_flat_invoice \
-    --ignore-table $DBNAME.sales_flat_invoice_comment \
-    --ignore-table $DBNAME.sales_flat_invoice_grid \
-    --ignore-table $DBNAME.sales_flat_invoice_item \
-    --ignore-table $DBNAME.sales_flat_order \
-    --ignore-table $DBNAME.sales_flat_order_address \
-    --ignore-table $DBNAME.sales_flat_order_grid \
-    --ignore-table $DBNAME.sales_flat_order_item \
-    --ignore-table $DBNAME.sales_flat_order_payment \
-    --ignore-table $DBNAME.sales_flat_order_status_history \
-    --ignore-table $DBNAME.sales_flat_quote \
-    --ignore-table $DBNAME.sales_flat_quote_address \
-    --ignore-table $DBNAME.sales_flat_quote_address_item \
-    --ignore-table $DBNAME.sales_flat_quote_item \
-    --ignore-table $DBNAME.sales_flat_quote_item_option \
-    --ignore-table $DBNAME.sales_flat_quote_payment \
-    --ignore-table $DBNAME.sales_flat_quote_shipping_rate \
-    --ignore-table $DBNAME.sales_flat_shipment \
-    --ignore-table $DBNAME.sales_flat_shipment_comment \
-    --ignore-table $DBNAME.sales_flat_shipment_grid \
-    --ignore-table $DBNAME.sales_flat_shipment_item \
-    --ignore-table $DBNAME.sales_flat_shipment_track \
-    --ignore-table $DBNAME.sales_invoiced_aggregated \
-    --ignore-table $DBNAME.sales_invoiced_aggregated_order \
-    --ignore-table $DBNAME.sales_order_aggregated_created \
-    --ignore-table $DBNAME.sales_order_aggregated_updated \
-    --ignore-table $DBNAME.sales_order_tax \
-    --ignore-table $DBNAME.sales_order_tax_item \
-    --ignore-table $DBNAME.sales_payment_transaction \
-    --ignore-table $DBNAME.sales_recurring_profile \
-    --ignore-table $DBNAME.sales_recurring_profile_order \
-    --ignore-table $DBNAME.sales_refunded_aggregated \
-    --ignore-table $DBNAME.sales_refunded_aggregated_order \
-    --ignore-table $DBNAME.sales_shipping_aggregated \
-    --ignore-table $DBNAME.sales_shipping_aggregated_order \
-    --ignore_table $DBNAME.customer_address_entity \
-    --ignore_table $DBNAME.customer_address_entity_datetime \
-    --ignore_table $DBNAME.customer_address_entity_decimal \
-    --ignore_table $DBNAME.customer_address_entity_int \
-    --ignore_table $DBNAME.customer_address_entity_text \
-    --ignore_table $DBNAME.customer_address_entity_varchar \
-    --ignore_table $DBNAME.customer_entity \
-    --ignore_table $DBNAME.customer_entity_decimal \
-    --ignore_table $DBNAME.customer_entity_int \
-    --ignore_table $DBNAME.customer_entity_text \
-    --ignore_table $DBNAME.customer_entity_varchar \
-    --ignore_table $DBNAME.wishlist \
-    --ignore_table $DBNAME.wishlist_item \
-    --ignore_table $DBNAME.wishlist_item_option \
+IGNORE_TABLES='';
+for excluded in "${SPECIFIC_IGNORED_TABLES[@]}"
+do
+IGNORE_TABLES="$IGNORE_TABLES --ignore-table=$DBNAME.$excluded "
+done
+for excluded in "${GLOBAL_IGNORED_TABLES[@]}"
+do
+IGNORE_TABLES="$IGNORE_TABLES --ignore-table=$DBNAME.$excluded "
+done
+
+nice -n 20 mysqldump -u$USERNAME -p$PASSWORD $DBNAME --no-data --compress --skip-lock-tables --verbose > $DBNAME.dump.sql
+nice -n 20 mysqldump -u$USERNAME -p$PASSWORD $DBNAME --no-create-db --no-create-info --single-transaction --quick --compress --skip-lock-tables --verbose \
+    $IGNORE_TABLES
     >> $DBNAME.dump.sql
 if [ "$GZIPFLAG" == 1 ]; then
     gzip $DBNAME.dump.sql
